3b9860e0d70d7acf78870ecd9916ed30472f3f09
triple141 <102752937+triple141@users.noreply.github.com>
2024-10-12 00:54:16 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,170 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // savarjisho 1 if ariyo sachiro
+//        int a = 5;
+//        int b = 10;
+//        int c;
+//
+//        c=b;
+//        b=a;
+//        a=c;
+//
+//        int m = 8;
+//        int n = 13;
+//
+//        n += m; //n = 21 ; m = 8
+//        m = n - m; // n = 21 ; m = 13
+//        n = n - m; // n = 21 - 13 = 8; m = 13;
+
+        // savarjisho 2 if araa sachiro
+//        double a=11.43567;
+//
+//        int b = (int)a;
+//
+//        System.out.println(a-b);
+
+        // savarjisho 3 if/case/? - is gareshe ver davwere )-;
+
+//        int a = 534;
+//
+//        if(a>99 & a<1000)
+//        {
+//            System.out.println("a is 3 digit number");
+//        }
+//        else
+//        {
+//            System.out.println("a isn't 3 digit number");
+//        }
+
+        // savarjisho 4
+
+//        char a = 'a';
+//
+//        a -= 32;
+//
+//        System.out.println(a);
+//        System.out.println((int)a);
+//
+//        if(a >= 97)
+//        {
+//            a -=32;
+//
+//            System.out.println(a);
+//        }
+
+        // savarjisho 5     if-is gareshe Honorable mention Math.round (-;
+
+//        double a=11.53567;
+//
+//        int b = (int)a;
+//
+//        double oper = a-b;
+//
+//        if(oper >= 0.5)
+//        {
+//            b++;
+//            System.out.println(b);
+//        }
+//        else
+//        {
+//            System.out.println(b);
+//        }
+
+        // savarjisho 6  !!!!import scanner  if-is gareshe vervqeni )-;
+
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.print("A (lengt of rectangle): ");
+//        float a = input.nextFloat();
+//
+//        System.out.print("B (width of rectangle): ");
+//        float b = input.nextFloat();
+//
+//        System.out.print("K (side of a scuare): ");
+//        float k = input.nextFloat();
+//
+//        int oper1;
+//        int oper2;
+//        if(a>k &  b>k)
+//        {
+//
+//            if(b%k == 0)
+//            {
+//                oper1 = (int)(b/k);
+//                oper2 = (int)(a/k +1);
+//                System.out.println(oper1 * oper2);
+//            }
+//            else if (a%k == 0)
+//            {
+//                oper1 = (int)(a/k);
+//                oper2 = (int)(b/k +1);
+//                System.out.println(oper1 * oper2);
+//            }
+//            else
+//            {
+//                oper1 = (int)(a/k +1);
+//                oper2 = (int)(b/k +1);
+//                System.out.println(oper1 * oper2);
+//            }
+//        }
+//        else if((a%k==0 & a>=k) & b%k==0 & b>=k)
+//        {
+//            oper1 = (int)(a/k);
+//            oper2 = (int)(b/k);
+//            System.out.println(oper1 * oper2);
+//        }
+//        else if ((a<k & b>k) | (a>k & b<k))
+//        {
+//            if(a>b)
+//            {
+//                if(a%k == 0)
+//                {
+//                    oper1 = (int)(a/k);
+//                    System.out.println(oper1);
+//                }
+//                else
+//                {
+//                    oper1 = (int) (a / k + 1);
+//                    System.out.println(oper1);
+//                }
+//            }
+//            else
+//            {
+//                if(b%k == 0)
+//                {
+//                    oper1 = (int)(b/k);
+//                    System.out.println(oper1);
+//                }
+//                else
+//                {
+//                    oper1 = (int) (b / k + 1);
+//                    System.out.println(oper1);
+//                }
+//            }
+//        }
+//        else
+//        {
+//            System.out.println("1");
+//        }
+
+        // savarjisho 7 if araa sachiro
+//        Scanner input = new Scanner(System.in);
+//
+//        int a = input.nextInt();
+//
+//        int hour;
+//        int min;
+//        int sec;
+//
+//       hour = a/3600;
+//       min = (a%3600)/60;
+//       sec = a-hour*3600-min*60;
+//
+//       System.out.println(hour + " : " + min + " : " + sec);
+//
+    }
+}
